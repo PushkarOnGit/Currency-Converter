7a17eb153bb89f364e9394536e6ea45a25749e96
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-06-29 23:49:35 +0530

    made the ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/currency_converter_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(home: CurrencyConverterPage());
   }
 }
--- lib/pages/currency_converter_page.dart
+++ lib/pages/currency_converter_page.dart
@@ -5,6 +5,55 @@ class CurrencyConverterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("demo Text")));
+    final border = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(30),
+      borderSide: BorderSide(width: 3, color: Color.fromARGB(255, 120, 0, 0)),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          'Currency Converter',
+          style: TextStyle(
+            color: Color.fromARGB(255, 0, 48, 73),
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 251, 231, 190),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "00",
+              style: TextStyle(
+                color: Color.fromARGB(255, 120, 0, 0),
+                fontSize: 55,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextField(
+                style: TextStyle(color: Colors.green[800], fontSize: 20),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                  signed: false,
+                ),
+                decoration: InputDecoration(
+                  enabledBorder: border,
+                  focusedBorder: border,
+                  prefixIcon: Icon(Icons.attach_money),
+                  prefixIconColor: Colors.green[800],
+                  hintText: 'Enter Amount in USD',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
