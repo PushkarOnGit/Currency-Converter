fc1a8650ff3ef23a7a3b0f98219c47e383aa75cd
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-12-02 19:12:30 +0530

    the UI is done

--- lib/pages/currency_converter_page.dart
+++ lib/pages/currency_converter_page.dart
@@ -5,6 +5,73 @@ class CurrencyConverterPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Hello World')));
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(color: Color.fromARGB(255, 120, 0, 0), width: 3.0),
+      borderRadius: BorderRadius.circular(12),
+    );
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 235, 195),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // ANSWER
+              Text(
+                '000',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 120, 0, 0),
+                  fontSize: 40,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // INPUT
+              TextField(
+                style: TextStyle(fontSize: 25),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Enter Amount in USD',
+                  hintStyle: TextStyle(color: Color.fromARGB(255, 120, 0, 0)),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 10,
+                  ),
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+              ),
+              SizedBox(height: 20),
+
+              //BUTTON
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  height: 55,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 120, 0, 0),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Convert',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 235, 195),
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
